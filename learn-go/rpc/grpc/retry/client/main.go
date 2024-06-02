4fc0b348412fb9ef7cdaaf0b055aaa798bb687a2
CaoChao <caochao222@gmail.com>
2024-06-02 15:42:38 +0800

    feat: 新增 grpc-retry demo

---
+++ learn-go/rpc/grpc/retry/client/main.go
@@ -0,0 +1,61 @@
+package main
+
+import (
+	"context"
+	"flag"
+	"log"
+	"time"
+
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials/insecure"
+	pb "learn-go/rpc/grpc/retry/proto"
+)
+
+var (
+	addr = flag.String("addr", "localhost:8051", "the address to connect to")
+	// see https://github.com/grpc/grpc/blob/master/doc/service_config.md to know more about service config
+	retryPolicy = `{
+		"methodConfig": [{
+		  "name": [{"service": "grpc.examples.echo.Echo"}],
+		  "waitForReady": true,
+		  "retryPolicy": {
+			  "MaxAttempts": 4,
+			  "InitialBackoff": ".01s",
+			  "MaxBackoff": ".01s",
+			  "BackoffMultiplier": 1.0,
+			  "RetryableStatusCodes": [ "UNAVAILABLE" ]
+		  }
+		}]}`
+)
+
+// use grpc.WithDefaultServiceConfig() to set service config
+func retryDial() (conn *grpc.ClientConn, err error) {
+	conn, err = grpc.Dial(*addr, grpc.WithTransportCredentials(insecure.NewCredentials()), grpc.WithDefaultServiceConfig(retryPolicy))
+	return
+}
+
+func main() {
+	flag.Parse()
+
+	// Set up a connection to the server.
+	conn, err := retryDial()
+	if err != nil {
+		log.Fatalf("did not connect: %v", err)
+	}
+	defer func() {
+		if e := conn.Close(); e != nil {
+			log.Printf("failed to close connection: %s", e)
+		}
+	}()
+
+	c := pb.NewEchoClient(conn)
+
+	ctx, cancel := context.WithTimeout(context.Background(), 1*time.Second)
+	defer cancel()
+
+	reply, err := c.UnaryEcho(ctx, &pb.EchoRequest{Message: "Try and Success"})
+	if err != nil {
+		log.Fatalf("UnaryEcho error: %v", err)
+	}
+	log.Printf("UnaryEcho reply: %v", reply)
+}
